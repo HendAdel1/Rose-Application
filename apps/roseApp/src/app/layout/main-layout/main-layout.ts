acf88b78bfe4e23ff3ce06adaad0db0c95ddbd30
abdelrahmaans <abdelrahmaan.aahmed@gmail.com>
2026-06-12 20:06:03 +0300

    refactor(rose-app): move layout templates to html

--- apps/roseApp/src/app/layout/main-layout/main-layout.ts
+++ apps/roseApp/src/app/layout/main-layout/main-layout.ts
@@ -6,15 +6,7 @@ import { Navbar } from '../navbar/navbar';
 @Component({
   imports: [Footer, Navbar, RouterOutlet],
   selector: 'app-main-layout',
-  template: `
-    <div class="app-layout">
-      <app-navbar></app-navbar>
-      <main class="app-content">
-        <router-outlet></router-outlet>
-      </main>
-      <app-footer></app-footer>
-    </div>
-  `,
+  templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
 export class MainLayout {}
