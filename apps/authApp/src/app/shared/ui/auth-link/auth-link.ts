03e8567464d2f81e0b28630b742e52f3aa4321a1
Hend Adel | Front-End Web Developer <86195780+HendAdel1@users.noreply.github.com>
2026-06-14 05:48:23 -0700

    Revert "Feature/login"

--- apps/authApp/src/app/shared/ui/auth-link/auth-link.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-auth-link',
-  imports: [],
-  templateUrl: './auth-link.html',
-  styleUrl: './auth-link.css',
-})
-export class AuthLink {}
