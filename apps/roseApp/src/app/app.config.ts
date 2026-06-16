93549d6fb42b3a529bc371ab49fbf58ff1274c47
abdelrahmaans <abdelrahmaan.aahmed@gmail.com>
2026-06-16 04:25:51 +0300

    Revert "chore: merge shared i18n setup into navbar branch"
    This reverts commit c828e1598c8aa78a0ed8d63dccd0dd7ab4640124, reversing
    changes made to 9e27d40edd8517245d59a4823ed862e1a8f85ccc.

--- apps/roseApp/src/app/app.config.ts
+++ apps/roseApp/src/app/app.config.ts
@@ -3,13 +3,8 @@ import {
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideSharedI18n } from '@org/shared-i18n';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes),
-    provideSharedI18n(),
-  ],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
 };
--- libs/shared-i18n/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/config/provide-shared-i18n';
-export * from './lib/shared-i18n/shared-i18n';
