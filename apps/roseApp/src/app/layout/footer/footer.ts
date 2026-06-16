33a51083cee2c5d5998844b949c536fd546425d8
abdelrahmaans <abdelrahmaan.aahmed@gmail.com>
2026-06-16 14:44:18 +0300

    fix: use reusable input in footer newsletter

--- apps/roseApp/src/app/layout/footer/footer.ts
+++ apps/roseApp/src/app/layout/footer/footer.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { CustomInput } from '../../../../../../libs/sharedComponents/src';
 
 @Component({
+  imports: [CustomInput],
   selector: 'app-footer',
   templateUrl: './footer.html',
   styleUrl: './footer.css',
