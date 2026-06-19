d610676f381fe088b9e87c46e4444df10fbb1a56
HendAdel1 <1hend.adel1@gmail.com>
2026-06-19 09:30:01 -0700

    add dark mode to header title h2

--- apps/authApp/src/app/pages/register/register.ts
+++ apps/authApp/src/app/pages/register/register.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [AuthLink,RegisterForm,RouterLink],
+  imports: [AuthLink, RegisterForm, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
