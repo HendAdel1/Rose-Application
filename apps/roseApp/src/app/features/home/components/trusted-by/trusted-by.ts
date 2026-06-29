e942dbd12f6378b32ade68747811cafdaf13ed8a
Salem Abd El Rahman <salemkamal977@gmail.com>
2026-06-29 16:07:46 +0300

    fix/trusted-by-translate

--- apps/roseApp/src/app/features/home/components/trusted-by/trusted-by.ts
+++ apps/roseApp/src/app/features/home/components/trusted-by/trusted-by.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {  TranslatePipe } from '@ngx-translate/core';
+import { SharedI18nService } from '@org/shared-i18n';
 
 @Component({
   selector: 'app-trusted-by',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './trusted-by.html',
   styleUrl: './trusted-by.css',
 })
-export class TrustedBy {}
+export class TrustedBy {
+  private readonly sharedI18nService=inject(SharedI18nService)
+  imagesUrl:string[]=['trusted/nut.png','trusted/ginyard.png','trusted/ingoud.png'
+    ,'trusted/velvet.png','trusted/goude.png','trusted/habu.png']
+}
