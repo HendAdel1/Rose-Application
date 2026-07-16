132ef2dd17a8a348a484e99046a128e0207b846d
Mathew-Samir <mthwsamir@gmail.com>
2026-07-16 19:29:05 +0300

    handle summary design and create product-like component

---
+++ apps/roseApp/src/app/features/cart/components/products-like/products-like.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-like',
+  imports: [],
+  templateUrl: './products-like.html',
+  styleUrl: './products-like.css',
+})
+export class ProductsLike {}
