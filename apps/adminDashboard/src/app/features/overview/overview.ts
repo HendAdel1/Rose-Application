e075efbdfd6665998bd647e8580b0afb48870b64
Salem Abd El Rahman <salemkamal977@gmail.com>
2026-08-31 21:11:59 +0300

    fix/dashboard-overview-page-Ui

--- apps/adminDashboard/src/app/features/overview/overview.ts
+++ apps/adminDashboard/src/app/features/overview/overview.ts
@@ -1,16 +1,17 @@
 import { Component, inject, OnInit, signal, ChangeDetectionStrategy } from '@angular/core';
-import { Category, OrderStatus, Revenue, Summary } from '../../../app/core/models/admin-statistics.model';
+import { Category, LowStockProduct, OrderStatus, Revenue, Summary, TopSellingProduct } from '../../../app/core/models/admin-statistics.model';
 import { Admin } from '../../core/services/admin/admin.service';
-import { DecimalPipe } from '@angular/common';
+import { CommonModule, DecimalPipe } from '@angular/common';
 import { LucideClipboardList, LucideDollarSign, LucidePackage, LucideReceipt } from '@lucide/angular';
 import { ChartModule } from 'primeng/chart';
 import { OrderStatusChart } from './orders-status-chart/orderStatusChart';
 import { RevenueChart } from './orders-status-chart/revenue-chart/revenueChart';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-admin-overview',
   standalone: true,
-  imports: [DecimalPipe,LucidePackage,LucideReceipt,LucideClipboardList,LucideDollarSign,ChartModule,OrderStatusChart,RevenueChart],
+  imports: [DecimalPipe,LucidePackage,LucideReceipt,LucideClipboardList,LucideDollarSign,ChartModule,OrderStatusChart,RevenueChart,CommonModule,TranslatePipe],
   templateUrl: './overview.html',
   styleUrl: './overview.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,6 +20,8 @@ export class Overview implements OnInit {
   private readonly adminService = inject(Admin);
   summaryData = signal<Summary | null>(null);
   categoriesData = signal<Category[]>([]);
+  topSellingProducts = signal<TopSellingProduct[]>([]);
+  lowStockProducts = signal<LowStockProduct[]>([]);
   orderStatusData = signal<OrderStatus | null>(null);
     revenueData = signal<Revenue | null>(null);
 
@@ -27,13 +30,15 @@ export class Overview implements OnInit {
   }
 
   getAllAdminStatistics(period: 'monthly' | 'week' = 'monthly'): void {
-    this.adminService.getAllAdminStatistics().subscribe({
+    this.adminService.getAllAdminStatistics(period).subscribe({
       next: (res: any) => {
-        if (res?.payload) {
-          this.summaryData.set(res.payload.summary);
-          this.categoriesData.set(res.payload.categories||[]);
-          this.orderStatusData.set(res.payload.orderStatus||null);
+        if (res) {
+          this.summaryData.set(res.summary);
+          this.categoriesData.set(res.categories||[]);
+          this.orderStatusData.set(res.orderStatus||null);
           this.revenueData.set(res.revenue);
+          this.topSellingProducts.set(res.topSellingProducts||[]);
+          this.lowStockProducts.set(res.lowStockProducts||[]);
 
         }
       },
