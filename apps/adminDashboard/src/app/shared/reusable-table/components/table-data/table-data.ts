d440566df95174faabdb094cb16992398ffd28a1
Mathew-Samir <mthwsamir@gmail.com>
2026-09-06 14:06:26 +0300

    Revert "reusable table structure"
    This reverts commit 018f9e34038d2faa048635abbe4b5e88df941829.

--- apps/adminDashboard/src/app/shared/reusable-table/components/table-data/table-data.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-table-data',
-  imports: [],
-  templateUrl: './table-data.html',
-  styleUrl: './table-data.css',
-})
-export class TableData {}
--- apps/adminDashboard/src/app/shared/reusable-table/components/table-search/table-search.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-table-search',
-  imports: [],
-  templateUrl: './table-search.html',
-  styleUrl: './table-search.css',
-})
-export class TableSearch {}
