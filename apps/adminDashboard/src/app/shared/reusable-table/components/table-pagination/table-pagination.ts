188c1af5ddfcda600318f2503f6fb3ea392c0fe0
Mathew-Samir <mthwsamir@gmail.com>
2026-09-06 14:49:40 +0300

    Revert "Revert "reusable table structure""
    This reverts commit d440566df95174faabdb094cb16992398ffd28a1.

---
+++ apps/adminDashboard/src/app/shared/reusable-table/components/table-pagination/table-pagination.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-pagination',
+  imports: [],
+  templateUrl: './table-pagination.html',
+  styleUrl: './table-pagination.css',
+})
+export class TablePagination {}
