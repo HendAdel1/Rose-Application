35ddc606201e3c1edff4b951595f24d54c5323ea
Mathew-Samir <mthwsamir@gmail.com>
2026-09-08 12:59:12 +0300

    feat: implement reusable table component

--- apps/adminDashboard/src/app/shared/reusable-table/reusable-table.ts
+++ apps/adminDashboard/src/app/shared/reusable-table/reusable-table.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TableData } from './components/table-data/table-data';
 import { TableSearch } from './components/table-search/table-search';
 import { TablePagination } from './components/table-pagination/table-pagination';
 
+/**
+ * Reusable Dynamic Table Component.
+ *
+ * Coordinates Search, TableData, and Pagination.
+ * Rendered in HTML simply as:
+ * ```html
+ * <app-reusable-table />
+ * ```
+ *
+ * Configuration is managed in TypeScript via `DataTableService`.
+ */
 @Component({
   selector: 'app-reusable-table',
   imports: [TableData, TableSearch, TablePagination],
   templateUrl: './reusable-table.html',
   styleUrl: './reusable-table.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReusableTable {}
